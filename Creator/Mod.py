4fe1aa6cc7fac38b0689e22da5ea9eea51759615
redbuttontheare <redbuttonthere@gmail.com>
2026-04-13 16:02:53 +0300

    Add files via upload

---
+++ Creator/Mod.py
@@ -0,0 +1,6 @@
+def registr(id, name):
+    print(f"Mod {name} was be loaded with ID: {id}")
+
+def license(studio, license):
+    print(f"© {studio} all rights reserved")
+    print(f"with license: {license}")
---
+++ Creator/__init__.py
@@ -0,0 +1,7 @@
+"""
+Custom local module for creating mods in this program
+© Heights studios© all rights reserved!
+"""
+
+# Todo: you can create mods with this code: from Creator.Mod import *
+# Todo: you must write 2 lines with code: regist(your mod id, your mod name) and license(your studio name, license)
